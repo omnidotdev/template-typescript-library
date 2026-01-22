c3672a34f225317d59712e484ff9578aaa422038
Brian Cooper <brian@brian-cooper.com>
2026-01-21 21:06:13 -0800

    feature: add initial files

---
+++ src/add.ts
@@ -0,0 +1,9 @@
+/**
+ * Adds two numbers.
+ * @param a First number
+ * @param b Second number
+ * @returns Sum of a and b
+ */
+const add = (a: number, b: number): number => a + b;
+
+export default add;
---
+++ src/subtract.ts
@@ -0,0 +1,9 @@
+/**
+ * Subtract two numbers.
+ * @param a First number
+ * @param b Second number
+ * @returns Difference of a and b
+ */
+const subtract = (a: number, b: number): number => a - b;
+
+export default subtract;
